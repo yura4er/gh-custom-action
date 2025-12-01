0fdaf4572b6a86b37fc589f500b06f5450dc3c59
Yurii Cheremishchuk <yurii.cheremishchuk@webpros.com>
2025-12-01 14:34:35 +0200

    Enhance S3 deployment action with input parameters and environment variables

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,17 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+    // 1. Get input values
+    const bucket = core.getInput("bucket", { required: true });
+    const region = core.getInput("region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
+    // 2. Upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
     core.notice("Hello from Deploy to S3 action!");
 }
 
