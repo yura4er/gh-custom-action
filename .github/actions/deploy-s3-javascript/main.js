0dc2236d03f48880b93f15089d7b823c7537ad78
Yurii Cheremishchuk <yurii.cheremishchuk@webpros.com>
2025-12-01 14:52:39 +0200

    Update default region to eu-north-1 in S3 deployment action and workflow

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,7 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
 
-    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    const websiteUrl = `http://${bucket}.s3-website.${region}.amazonaws.com`;
     core.setOutput("website-url", websiteUrl);
     core.info(`Website deployed at: ${websiteUrl}`);
 }
